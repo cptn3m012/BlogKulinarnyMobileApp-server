ec35159ad2173566b78ae2a4adf69b9ca0d311bd
Desi451 <mdesortes00@gmail.com>
2023-06-02 03:18:42 +0200

    Added recipes download from database

--- main.py
+++ main.py
@@ -74,6 +74,63 @@ def register():
     finally:
         conn.close()  # Zamknięcie połączenia z bazą danych
 
+@app.route('/loadRecipes', methods=['GET'])
+def loadRecipes():
+    # Połączenie z bazą danych
+    server = 'YOUR_SERVER_HERE'
+    database = 'YOUR_DATABASE_NAME_HERE'
+    login = 'YOUR_LOGIN_HERE'
+    password = 'YOUR_PASSWORD_HERE'
+    driver = '{ODBC Driver 17 for SQL Server}'
+    conn_str = f"DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={login};PWD={password}"
+    conn = pyodbc.connect(conn_str)
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT * FROM [recipes] AS [r] WHERE [r].[isAccepted] = 1")
+
+        recipes = []
+        current_recipe_id = None
+        recipe = None
+
+        for row in cursor.fetchall():
+            recipe_id, is_accepted, title, image_url, description, difficulty, avg_time, portions, user_id, category_id = row
+
+            if recipe_id != current_recipe_id:
+                if recipe is not None:
+                    recipes.append(recipe)
+                current_recipe_id = recipe_id
+                recipe = {
+                    "id": recipe_id,
+                    "isAccepted": is_accepted,
+                    "title": title,
+                    "imageURL": image_url,
+                    "description": description,
+                    "difficulty": difficulty,
+                    "avgTime": avg_time,
+                    "portions": portions,
+                    "userId": user_id,
+                    "CategoryId": category_id
+                }
+
+            if category_id is not None:
+                category = {
+                    "id": category_id,
+                    "name": category_name
+                }
+
+        if recipe is not None:
+            recipes.append(recipe)
+
+        conn.close()
+        return jsonify({'recipes': recipes}), 200
+    except Exception as e:
+        print(e)
+        conn.rollback()
+        return jsonify({'error': 'Wystąpił błąd podczas pobierania przepisów.'}), 500
+
+
 #main
 if __name__ == '__main__':
     # Uruchomienie aplikacji Flask z obsługą HTTP
